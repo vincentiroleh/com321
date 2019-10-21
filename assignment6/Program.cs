ea62db0ab7b0944d00085a33224f79fb871106e3
Vincent Iroleh <irolehiroleh@gmail.com>
2019-10-21 12:56:05 +0100

    Write a program to compute the product AB, store the result in P and subsequently print P.

---
+++ assignment6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                Write a program to compute the product AB, store the result in P and subsequently print P.
+            */
+510            int productA;
+            int productB;
+            int resultP;
+
+            Console.WriteLine("Enter product's A value: ");
+            productA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter product's B value: ");
+            productB = Convert.ToInt32(Console.ReadLine());
+
+            resultP = (productA + productB);
+            Console.WriteLine($"The sum of {productA} and {productB} is {resultP}");
+
+        }
+    }
+}
