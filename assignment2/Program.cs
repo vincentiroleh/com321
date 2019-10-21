dd658ae6bb64ea0f2d0d0e42e82dd14cc9279626
Vincent Iroleh <irolehiroleh@gmail.com>
2019-10-21 09:35:15 +0100

    Write a program to read the elements of a 2 by 3 matrices

---
+++ assignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                Write a program to read the elements of a 2 by 3 matrices 
+                as given by A =
+             
+                8          5           1
+                7          2           6
+            */
+            int[,] intArray = new int [2, 3] 
+            {
+                {8, 5, 1},
+                {7, 2, 6}
+            };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j =0; j < 3; j++) 
+                {
+                    Console.Write(intArray[i,j]+ "\t");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
